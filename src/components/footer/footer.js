fbceed88d6ddc04a4b41bad32b105cb4789ad9e0
Deepanshi <200104071@hbtu.ac.in>
2022-09-20 21:57:08 +0530

    First Commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+       
+          Deepanshi
+        
+      </span>
+      <div className="iconContainer">
+        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+          <i className="fab fa-youtube fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
